9da9b6488173b7ff96cc4322d2e42ca4198f5d85
walaafikre <walaafikre@users.noreply.github.com>
2021-03-05 20:30:42 +0200

    Update stories_model.dart

--- lib/model/stories_model.dart
+++ lib/model/stories_model.dart
@@ -1,82 +1,50 @@
 const List stories_model = [
   {
-    "img":
-    "https://images.unsplash.com/photo-1611867416365-f636328625fa?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDI0fHRvd0paRnNrcEdnfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "name": "David Hinkle",
-    "nickname": "david_101",
-    "date": "Thu",
-    "videoUrl": "assets/video_4.mp4"
+    'img':
+        'https://images.unsplash.com/photo-1546641082-017d1cdf8893?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDN8QkpKTXR0ZURKQTR8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    'name': 'Moms',
+    'nickname': 'Moms',
+    'date': 'Thu',
+    'videoUrl': "assets/movies/video_4.mp4"
   },
   {
-    "img":
-    "https://images.unsplash.com/photo-1611467377820-6b374841dc5f?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDExN3x0b3dKWkZza3BHZ3x8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "name": "Charle Chen",
-    "nickname": "chen007",
-    "date": "Sun",
-    "videoUrl": "assets/video_1.mp4"
+    'img':
+        'https://images.unsplash.com/photo-1614670521939-b600cc054a44?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8QkpKTXR0ZURKQTR8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    'name': 'Marco Zuppone',
+    'nickname': 'Marco012',
+    'date': 'Sun',
+    'videoUrl': 'assets/movies/video_1.mp4'
   },
   {
-    "img":
-    "https://images.unsplash.com/photo-1560148618-a335d1473ab1?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDE0Mnx0b3dKWkZza3BHZ3x8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "name": "Maria Tenve",
-    "nickname": "tenve_maria",
-    "date": "Today",
-    "videoUrl": "assets/video_6.mp4"
+    'img':
+        'https://images.unsplash.com/photo-1614407556955-da46ab6b31dc?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDEyfFM0TUtMQXNCQjc0fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    'name': 'Lili',
+    'nickname': 'Lili444',
+    'date': 'Today',
+    'videoUrl': 'assets/movies/video_6.mp4'
   },
   {
-    "img":
-    "https://images.unsplash.com/photo-1611901041222-db4b69b798f2?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDI2fHRvd0paRnNrcEdnfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "name": "Dimitry Zub",
-    "nickname": "Zub",
-    "date": "4h ago",
-    "videoUrl": "assets/video_5.mp4"
+    'img':
+        'https://images.unsplash.com/photo-1613395866040-e7d946fd8097?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDM3fEJKSk10dGVESkE0fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    'name': 'Mom',
+    'nickname': 'Mom',
+    'date': 'Thu',
+    'videoUrl': "assets/movies/video_4.mp4"
   },
   {
-    "img":
-    "https://images.unsplash.com/photo-1611948357031-ef7bce1e44f0?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDI1fHRvd0paRnNrcEdnfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "name": "Sajad Nori",
-    "nickname": "sajad_nori",
-    "date": "Mon",
-    "videoUrl": "assets/video_2.mp4"
+    'img':
+        'https://images.unsplash.com/photo-1614670521939-b600cc054a44?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8QkpKTXR0ZURKQTR8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    'name': 'Marco Zuppone',
+    'nickname': 'Marco012',
+    'date': 'Sun',
+    'videoUrl': 'assets/movies/video_1.mp4'
   },
   {
-    "img":
-    "https://images.unsplash.com/photo-1612065010776-ede094ba6204?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDMxfHRvd0paRnNrcEdnfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "name": "Lucas Mend",
-    "nickname": "Mendas005",
-    "date": "Today",
-    "videoUrl": "assets/video_3.mp4"
+    'img':
+        'https://images.unsplash.com/photo-1614407556955-da46ab6b31dc?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDEyfFM0TUtMQXNCQjc0fHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+    'name': 'Lili',
+    'nickname': 'Lili444',
+    'date': 'Today',
+    'videoUrl': 'assets/movies/video_6.mp4'
   },
-  {
-    "img":
-    "https://images.unsplash.com/photo-1548142542-c53707f8b05b?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDU2fHRvd0paRnNrcEdnfHxlbnwwfHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "name": "Joel Mott",
-    "nickname": "mott_joel",
-    "date": "Mon",
-    "videoUrl": "assets/video_1.mp4"
-  },
-  {
-    "img":
-    "https://images.unsplash.com/photo-1610186594416-2c7c0131e35d?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDEwN3x0b3dKWkZza3BHZ3x8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "name": "Riccardo Pitz",
-    "nickname": "ptzriccardo001",
-    "date": "1h ago",
-    "videoUrl": "assets/video_3.mp4"
-  },
-  {
-    "img":
-    "https://images.unsplash.com/photo-1611442535630-38c6b493bee0?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDExM3x0b3dKWkZza3BHZ3x8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "name": "Altin Feriha",
-    "nickname": "feriha",
-    "date": "Wed",
-    "videoUrl": "assets/video_4.mp4"
-  },
-  {
-    "img":
-    "https://images.unsplash.com/photo-1611149469655-6706927f7335?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDExNHx0b3dKWkZza3BHZ3x8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-    "name": "Mike Palm",
-    "nickname": "mike_palm",
-    "date": "Wed",
-    "videoUrl": "assets/video_1.mp4"
-  }
 ];
