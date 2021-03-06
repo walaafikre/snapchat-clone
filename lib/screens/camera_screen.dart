96d3925c0c5605d19501171262014abf3050b190
walaafikre <walaafikre@users.noreply.github.com>
2021-03-06 19:24:21 +0200

    Update camera_screen.dart

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -1,4 +1,11 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:flutter_snapchat_clone_full/screens/preview_screen.dart';
+import 'package:flutter_snapchat_clone_full/screens/profile_screen.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 class CameraScreen extends StatefulWidget {
   @override
@@ -6,8 +13,366 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
+  CameraController controller;
+  List cameras;
+  int selectedCameraIndex;
+  String imgPath;
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    availableCameras().then((availableCameras) {
+      cameras = availableCameras;
+
+      if (cameras.length > 0) {
+        setState(() {
+          selectedCameraIndex = 0;
+        });
+        _initCameraController(cameras[selectedCameraIndex]).then((void v) {});
+      } else {
+        print('No camera available');
+      }
+    }).catchError((err) {
+      print('Error :${err.code}Error message : ${err.message}');
+    });
+  }
+
+  Future _initCameraController(CameraDescription cameraDescription) async {
+    if (controller != null) {
+      await controller.dispose();
+    }
+    controller = CameraController(cameraDescription, ResolutionPreset.high);
+
+    controller.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+
+      if (controller.value.hasError) {
+        print('Camera error ${controller.value.errorDescription}');
+      }
+    });
+
+    try {
+      await controller.initialize();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+    }
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      key: _scaffoldKey,
+      backgroundColor: Colors.black,
+      floatingActionButton: header(context),
+      body: camera(context),
+    );
+  }
+
+  Widget cameraPreview() {
+    return AspectRatio(
+      aspectRatio: controller.value.aspectRatio,
+      child: CameraPreview(controller),
+    );
+  }
+
+  IconData _getCameraLensIcon(CameraLensDirection direction) {
+    switch (direction) {
+      case CameraLensDirection.back:
+        return SimpleLineIcons.refresh;
+      case CameraLensDirection.front:
+        return SimpleLineIcons.refresh;
+      case CameraLensDirection.external:
+        return Icons.camera;
+      default:
+        return Icons.device_unknown;
+    }
+  }
+
+  void _onSwitchCamera() {
+    selectedCameraIndex =
+        selectedCameraIndex < cameras.length - 1 ? selectedCameraIndex + 1 : 0;
+    CameraDescription selectedCamera = cameras[selectedCameraIndex];
+    _initCameraController(selectedCamera);
+  }
+
+  String timestamp() => DateTime.now().millisecondsSinceEpoch.toString();
+
+  Future<void> _onCameraSwitched(CameraDescription cameraDescription) async {
+    if (controller != null) {
+      await controller.dispose();
+    }
+
+    controller = CameraController(cameraDescription, ResolutionPreset.high);
+
+    // If the controller is updated then update the UI.
+    controller.addListener(() {
+      if (mounted) {
+        setState(() {});
+      }
+
+      if (controller.value.hasError) {
+        Fluttertoast.showToast(
+            msg: 'Camera error ${controller.value.errorDescription}',
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white);
+      }
+    });
+
+    try {
+      await controller.initialize();
+    } on CameraException catch (e) {
+      _showCameraException(e);
+    }
+
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  void _showCameraException(CameraException e) {
+    String errorText = 'Error: ${e.code}\nError Message: ${e.description}';
+    print(errorText);
+
+    Fluttertoast.showToast(
+        msg: 'Error: ${e.code}\n${e.description}',
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white);
+  }
+
+  Widget cameraTogglesRowWidget() {
+    if (cameras == null || cameras.isEmpty) {
+      return Spacer();
+    }
+    CameraDescription selectedCamera = cameras[selectedCameraIndex];
+    CameraLensDirection lensDirection = selectedCamera.lensDirection;
+
+    return Align(
+      alignment: Alignment.topLeft,
+      child: InkWell(
+          onTap: _onSwitchCamera,
+          child: Icon(
+            _getCameraLensIcon(lensDirection),
+            color: Colors.white,
+          )),
+    );
+  }
+
+  Widget camera(context) {
+    var size = MediaQuery.of(context).size;
+    if (controller == null || !controller.value.isInitialized) {
+      return const Text(
+        'Loading',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w900,
+        ),
+      );
+    }
+    return Container(
+      width: size.width,
+      height: size.height,
+      child: ClipRRect(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.circular(10)),
+          child: cameraPreview()),
+    );
+  }
+
+  void _onCapturePressed(context) async {
+    try {
+      final path =
+          join((await getTemporaryDirectory()).path, '${DateTime.now()}.png');
+      await controller.takePicture(path);
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => PreviewScreen(
+                  imgPath: path,
+                )),
+      );
+    } catch (e) {
+      _showCameraException(e);
+    }
+  }
+
+  Widget header(context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 30, top: 60),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            flex: 0,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.black.withOpacity(0.3)),
+                      child: Icon(
+                        Entypo.user,
+                        color: Colors.yellow,
+                        size: 23,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.black.withOpacity(0.3)),
+                      child: Icon(
+                        Feather.search,
+                        color: Colors.white,
+                        size: 23,
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => ProfileScreen()));
+                      },
+                      child: Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.black.withOpacity(0.3)),
+                        child: Icon(
+                          Feather.user_plus,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.3,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.black.withOpacity(0.3)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            cameraTogglesRowWidget(),
+                            SizedBox(
+                              height: 18,
+                            ),
+                            Icon(
+                              Entypo.flash,
+                              color: Colors.white,
+                              size: 25,
+                            ),
+                            SizedBox(
+                              height: 18,
+                            ),
+                            Icon(
+                              MaterialCommunityIcons.play_box_outline,
+                              color: Colors.white,
+                              size: 25,
+                            ),
+                            SizedBox(
+                              height: 18,
+                            ),
+                            Icon(
+                              Entypo.beamed_note,
+                              color: Colors.white,
+                              size: 25,
+                            ),
+                            SizedBox(
+                              height: 18,
+                            ),
+                            Icon(
+                              Ionicons.ios_arrow_down,
+                              color: Colors.white,
+                              size: 25,
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    MaterialCommunityIcons.cards_playing_outline,
+                    color: Colors.white,
+                    size: 28,
+                  ),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    _onCapturePressed(context);
+                  },
+                  child: Container(
+                    width: 80,
+                    height: 80,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(width: 7, color: Colors.white)),
+                  ),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                    Entypo.emoji_happy,
+                    color: Colors.white,
+                    size: 28,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
